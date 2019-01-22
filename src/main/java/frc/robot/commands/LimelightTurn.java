3aa31ca708f561f16db029c9731e658f27479e6b
Jacob4649 <jacob.klimczak@gmail.com>
2019-01-22 15:16:27 -0500

    fixed up limelightTurn

--- src/main/java/frc/robot/commands/LimelightTurn.java
+++ src/main/java/frc/robot/commands/LimelightTurn.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.command.PIDCommand;
 import frc.robot.Robot;
 import frc.robot.RobotConstants;
 
+//TODO : Tuning
+
 /**
  * Turns the robot so that it is aligned with whatever the robot is detecting on the limelight
  * @author Jacob4649
@@ -30,11 +32,16 @@ public class LimelightTurn extends PIDCommand {
     requires(Robot.m_driveTrain);
 
     this.m_maxSpeed = maxSpeed;
+    getPIDController().setInputRange(-20.5, 20.5);
+    getPIDController().setOutputRange(0.2, 0.8); //Dummy numbers, will need to be updates
+    getPIDController().setContinuous(false);
+    getPIDController().setAbsoluteTolerance(4.0);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    getPIDController().enable();
   }
 
   // Called repeatedly when this Command is scheduled to run
@@ -45,7 +52,7 @@ public class LimelightTurn extends PIDCommand {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return getPIDController().onTarget();
   }
 
   // Called once after isFinished returns true
@@ -64,7 +71,7 @@ public class LimelightTurn extends PIDCommand {
   // returns the value the pid controller is using as an input
   @Override
   protected double returnPIDInput() {
-    return 0; // returns the distance from center of the object detected by the limelight
+    return Robot.m_limelight.getOffset()[0]; // returns the horizontal distance from center of the object detected by the limelight
   }
 
   // processes the pid output, sends new values to motors and stuff
