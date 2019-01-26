0bdf43a96cb213c930cc514c9c959d289fa90254
Jacob4649 <jacob.klimczak@gmail.com>
2019-01-26 09:47:20 -0500

    Auto

--- src/main/java/frc/robot/commands/drivetrain/AlignWithTarget.java
+++ src/main/java/frc/robot/commands/drivetrain/AlignWithTarget.java
@@ -7,26 +7,43 @@
 
 package frc.robot.commands.drivetrain;
 
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.command.PIDCommand;
 import frc.robot.Robot;
 
 //TODO : Tuning
 
-public class AlignWithTarget extends PIDCommand {
+public class AlignWithTarget extends PIDCommand implements PIDOutput {
+  
+  PIDController gyroController;
+  DummyOutput dummyOutput;
 
   public AlignWithTarget() {
-    super(0.2, 0.0, 0.0);
+
+    super(0.001, 0.001, 0.0);
     requires(Robot.m_driveTrain);
 
+    dummyOutput = new DummyOutput();
+
+    gyroController = new PIDController(0.009, 0.0, 0.0, Robot.m_driveTrain.getGyro(), dummyOutput);
+    gyroController.setInputRange(0, 360.0);
+    gyroController.setContinuous(true);
+    gyroController.setOutputRange(-0.2, 0.2);
+    gyroController.setSetpoint(Robot.m_driveTrain.getGyroAngle());
+    gyroController.setAbsoluteTolerance(4);
+
     getPIDController().setInputRange(-20.5, 20.5);
-    getPIDController().setOutputRange(0.2, 0.8); // Dummy numbers, will need to be updates
+    getPIDController().setOutputRange(-0.2, 0.2); 
     getPIDController().setContinuous(false);
-    getPIDController().setAbsoluteTolerance(4.0);
+    getPIDController().setAbsoluteTolerance(1.0);
+    getPIDController().setSetpoint(0.0);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    gyroController.enable();
     getPIDController().enable();
   }
 
@@ -38,13 +55,16 @@ public class AlignWithTarget extends PIDCommand {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return getPIDController().onTarget();
+    //return getPIDController().onTarget();
+    return false;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
     Robot.m_driveTrain.stop();
+    gyroController.disable();
+    getPIDController().disable();
   }
 
   // Called when another command which requires one or more of the same
@@ -61,6 +81,21 @@ public class AlignWithTarget extends PIDCommand {
 
   @Override
   public void usePIDOutput(double output) {
-    Robot.m_driveTrain.mecanumDrive(0.0, output, 0.0);
+    Robot.m_driveTrain.mecanumDrive(0.0, -output, gyroController.get());
+  }
+
+    
+  @Override
+  public void pidWrite(double output) {
+    //whatever
+  }
+
+}
+
+class DummyOutput implements PIDOutput {
+  
+  @Override
+  public void pidWrite(double output) {
+    //whatever
   }
 }
