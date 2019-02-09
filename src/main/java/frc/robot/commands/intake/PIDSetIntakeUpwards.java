8dfcd764c7e013bf58615b691140b3f0c87825de
Matt Reynolds <mtreynolds@uwaterloo.ca>
2019-02-09 06:57:56 -0800

    Re-added arm actuation

---
+++ src/main/java/frc/robot/commands/intake/PIDSetIntakeUpwards.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.intake;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import frc.robot.Robot;
+import frc.robot.RobotConstants;
+
+public class PIDSetIntakeUpwards extends PIDCommand {
+
+  double m_maxSpeed, m_place;
+ boolean m_value;
+ PIDController m_intakeAnalogPot;
+
+  public PIDSetIntakeUpwards(double maxSpeed, double place, boolean value) {
+   super (0.02, 0,0);
+   requires(Robot.m_intake);
+
+    m_maxSpeed = maxSpeed;
+   m_place = place;
+   m_value = value;
+
+  }
+ @Override
+ protected void initialize() {
+   setSetpoint(RobotConstants.UPWARDS_INTAKE_POSITION);    
+ }
+
+  @Override
+ protected void execute() {
+ }
+
+  @Override
+ protected boolean isFinished() {
+   return false;
+ }
+
+  @Override
+ protected void end() {
+ }
+
+
+  @Override
+ protected void interrupted() {
+ }
+
+  protected double returnPIDInput() {
+   return Robot.m_intake.getAnalogPot().get();   
+ }
+
+  protected void usePIDOutput(double output){
+   Robot.m_intake.getAnalogPotMotor().set(ControlMode.PercentOutput, output);
+
+  }
+
+}
