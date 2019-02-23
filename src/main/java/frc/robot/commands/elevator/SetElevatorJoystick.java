3db12544db2d2176581fd9eff0e7a8d4339a9efe
SwuntiiTHICC <ZHENGSTA7@gmail.com>
2019-02-23 12:53:51 -0500

    dumb first draft elevator garbage

--- src/main/java/frc/robot/commands/elevator/SetElevatorJoystick.java
+++ src/main/java/frc/robot/commands/elevator/SetElevatorJoystick.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotConstants;
 import frc.robot.Utilities;
+import frc.robot.commands.grabberarm.SetArmToPos;
 
 public class SetElevatorJoystick extends Command {
 
@@ -26,18 +27,30 @@ public class SetElevatorJoystick extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    /*
-    if(Robot.m_elevator.getHeight() >= RobotConstants.ELEVATOR_MAXIMUM_HEIGHT && 
-        Robot.m_oi.getDriveStick().getRawAxis(1) >= 0 
-      || Robot.m_elevator.getHeight() <= RobotConstants.ELEVATOR_MINIMUM_HEIGHT &&
-        Robot.m_oi.getDriveStick().getRawAxis(1) <= 0) {
-         
-      Robot.m_elevator.setElevator(0);
-    } else { 
-      
+    if(Robot.m_elevator.getHeight() <= RobotConstants.ELEVATOR_MAXIMUM_HEIGHT && 
+      Robot.m_oi.getDriveStick().getRawAxis(1) > 0 
+      || Robot.m_elevator.getHeight() >= RobotConstants.ELEVATOR_MINIMUM_HEIGHT &&
+      Robot.m_oi.getDriveStick().getRawAxis(1) <= 0) {
+     
+    Robot.m_elevator.setElevator(0);
+
+  } else { 
+  
+    Robot.m_elevator.setElevator(Utilities.deadzone(-Robot.m_oi.getDriveStick().getRawAxis(1)*1.0));
+
+    if(Robot.m_elevator.getHeight() >= RobotConstants.ELEVATOR_NO_CONFLICT_HEIGHT - 0.1 && Robot.m_oi.getDriveStick().getRawAxis(1) > 0
+      && (Robot.m_grabberArm.getPos() < RobotConstants.GRABBER_ARM_HOLD_BALL || Robot.m_grabberArm.getPos() > RobotConstants.GRABBER_ARM_HOLD_HATCH)
+      && Robot.m_grabber.getGrabberIn()) {
+
+        new SetArmToPos(RobotConstants.GRABBER_ARM_HOLD_BALL, 0.8).start();
+
+    } else if (Robot.m_elevator.getHeight() >= RobotConstants.ELEVATOR_NO_CONFLICT_HEIGHT - 0.1 && Robot.m_oi.getDriveStick().getRawAxis(1) > 0
+      && (Robot.m_grabberArm.getPos() < RobotConstants.GRABBER_ARM_HOLD_HATCH || Robot.m_grabberArm.getPos() > RobotConstants.GRABBER_ARM_OUT - 1.5)
+      && Robot.m_grabber.getGrabberOut()) {
+
+        new SetArmToPos(RobotConstants.GRABBER_ARM_HOLD_HATCH, 0.8).start();
+      }
     }
-    */
-    Robot.m_elevator.setElevator(Utilities.deadzone(-Robot.m_oi.getOperatorStick().getRawAxis(1)*1.0));
   }
 
   // Make this return true when this Command no longer needs to run execute()
